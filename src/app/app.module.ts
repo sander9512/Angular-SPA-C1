87a60900b4051e646baace61b08bd4748ee527e9
Kayvon Rahimi Morad Ali <krahimim@avans.nl>
2017-12-19 12:23:46 +0100

    Added dropdown
    Dropdown on header added for future use

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import {DropdownDirective} from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    DropdownDirective,
   ],
   imports: [
     BrowserModule
