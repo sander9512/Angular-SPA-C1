cc30464a58daedab86eeba41163a3013204ed8b5
sander9512 <s.c.schep@gmail.com>
2017-12-22 00:38:19 +0100

    Sportshall service for retrieving all or a specific sportshall

--- src/app/shared/services/sportshall.service.ts
+++ src/app/shared/services/sportshall.service.ts
@@ -1,8 +1,16 @@
 import { SportsHall } from '../models/sportshall.model';
 import { Injectable } from '@angular/core';
+import {Subject} from 'rxjs/Subject';
+import {environment} from '../../../environments/environment';
+import {Http, Headers} from '@angular/http';
+import {toPromise} from 'rxjs/operator/toPromise';
 
 @Injectable()
-export class SportsHallsService{
+export class SportsHallsService {
+  hallsChanged = new Subject<SportsHall[]>();
+  private headers = new Headers({'Content-Type': 'application/json'});
+  private serverUrl = environment.serverUrl + '/halls'; // URL to web api
+  private halls: SportsHall[];
   private sportsHalls: SportsHall[] = [
     new SportsHall(
         'Sporty Times',
@@ -27,8 +35,34 @@ export class SportsHallsService{
     )
   ];
 
+  constructor(private http: Http) {
+  }
   getSportsHalls() {
     return this.sportsHalls.slice();
+  }
+  _getSportsHalls(): Promise<SportsHall[]> {
+    return this.http.get(this.serverUrl, {headers: this.headers})
+      .toPromise()
+      .then(response => {
+        console.dir(response.json());
+        this.halls = response.json() as SportsHall[];
+        return this.halls;
+      })
+      .catch(error => {
+        console.log('handleError');
+        return Promise.reject(error.message || error);
+      });
+  }
+  _getSportsHall(id: number): Promise<SportsHall> {
+    return this.http.get(this.serverUrl + '/' + id, {headers: this.headers})
+      .toPromise()
+      .then(response => {
+        return response.json() as SportsHall;
+      })
+      .catch(error => {
+        console.log(error);
+        return Promise.reject(error.message || error);
+      });
   }
 
   getSportsHall(index: number) {
